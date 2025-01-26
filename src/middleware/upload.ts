0401a36bbf0edd419641ca42ec8842106fc7da86
kandari_nitesh <niteshskandari@gmail.com>
2025-01-26 18:19:17 +0530

    updated

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,11 +1,9 @@
 import multer from "multer";
 import path from "path";
-import fs from "fs/promises";
 
-console.log(__dirname, "check")
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, "../uploads"));
+    cb(null,"uploads/");
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`); // Unique filename with timestamp
