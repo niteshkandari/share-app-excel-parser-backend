614a130ca087a4f455963d1e0562f5feca9be994
kandari_nitesh <niteshskandari@gmail.com>
2025-01-26 18:22:52 +0530

    updated

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,21 +1,23 @@
 import multer from "multer";
 import path from "path";
 
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null,"uploads/");
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`); // Unique filename with timestamp
+    cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
 
 const fileFilter = (req: any, file: any, cb: any) => {
   const ext = path.extname(file.originalname);
   if (ext !== ".xlsx" && ext !== ".xls" && ext !== ".numbers" && ext !== ".csv") {
-    return cb(new Error("Only Excel, CSV, and .numbers files are allowed!"));
+    return cb(new Error("Only Excel, csv and .numbers files are allowed!"));
   }
   cb(null, true);
 };
+
 
 export const upload = multer({ storage, fileFilter });
