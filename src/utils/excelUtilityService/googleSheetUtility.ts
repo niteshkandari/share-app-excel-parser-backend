d79bdb8112740518440bdf0fae6a1f718d7e1da0
kandari_nitesh <niteshskandari@gmail.com>
2025-01-27 00:21:11 +0530

    updated google auth cred

--- src/utils/excelUtilityService/googleSheetUtility.ts
+++ src/utils/excelUtilityService/googleSheetUtility.ts
@@ -1,17 +1,28 @@
-import { google } from "googleapis";
+import { google, sheets_v4 } from "googleapis";
 import { JWT } from "google-auth-library";
+import path from "path";
 
 export async function updateGoogleSheet(SHEET_ID: string, headers: string[], data: string[][], range = "Sheet1!A1") {
   try {
-    const auth = new JWT({
-      key: 
-        (process.env.GOOGLE_PRIVATE_KEY as string)?.replace(/\\n/g, '\n')
-      ,
-      email: process.env.GOOGLE_CLIENT_EMAIL,
+    // const auth = new JWT({
+    //   key: 
+    //     process.env.GOOGLE_PRIVATE_KEY 
+    //   ,
+    //   email: process.env.GOOGLE_CLIENT_EMAIL,
+    //   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    // });
+
+    const decodedServiceAccount = Buffer.from(process.env.ENCODED_SECRET!, 'base64').toString('utf-8');
+    const credentials = JSON.parse(decodedServiceAccount);
+    console.log(credentials , "check")
+    const auth = new google.auth.GoogleAuth({
+      credentials,
       scopes: ["https://www.googleapis.com/auth/spreadsheets"],
     });
+    // Get the authorized client
+    const authClient = await auth.getClient() as sheets_v4.Options["auth"];
 
-    const sheets = google.sheets({ version: "v4", auth });
+    const sheets = google.sheets({ version: "v4", auth:authClient });
     const values = [[...headers], ...data];
     const response: any = await sheets.spreadsheets.values.update({
       spreadsheetId: SHEET_ID,
