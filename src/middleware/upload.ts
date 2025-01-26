afef35d27c5d75d4dd120076e4f5cd65fa9283e2
kandari_nitesh <niteshskandari@gmail.com>
2025-01-26 19:23:46 +0530

    updated multer file

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,45 +1,14 @@
-// import multer from "multer";
-// import path from "path";
-// import fs from "fs";
-
-
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     const uploadPath = path.resolve("uploads");
-//     if (!fs.existsSync(uploadPath)) {
-//       fs.mkdirSync(uploadPath);
-//     }
-//     cb(null, "uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, `${Date.now()}-${file.originalname}`);
-//   },
-// });
-
-// const fileFilter = (req: any, file: any, cb: any) => {
-//   const ext = path.extname(file.originalname);
-//   if (ext !== ".xlsx" && ext !== ".xls" && ext !== ".numbers" && ext !== ".csv") {
-//     return cb(new Error("Only Excel, csv and .numbers files are allowed!"));
-//   }
-//   cb(null, true);
-// };
-
-// export const upload = multer({ storage, fileFilter });
-
-
 import multer from "multer";
 import path from "path";
 import fs from "fs";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // const uploadPath = path.resolve("/tmp");
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
+  // destination: (req, file, cb) => {
+  //   cb(null, "uploads/");
+  // },
+  // filename: (req, file, cb) => {
+  //   cb(null, `${Date.now()}-${file.originalname}`);
+  // },
 });
 
 const fileFilter = (req: any, file: any, cb: any) => {
