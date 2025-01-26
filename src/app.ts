879fd3a816bdb65fc669f5418d4a7399e36ca716
kandari_nitesh <niteshskandari@gmail.com>
2025-01-26 22:09:13 +0530

    updated

--- src/app.ts
+++ src/app.ts
@@ -9,14 +9,7 @@ import { errorMiddleware } from './middleware/errors';
 
 export default async (app:Express) => {
 
-  app.use(cors(
-    {
-      origin: [
-        'http://localhost:3000', // Local development
-        'https://share-app-frontend-ll64s0qzl-niteshs-projects-21802273.vercel.app', // Deployed frontend
-      ],
-    }
-  ));
+  app.use(cors());
   app.use(express.json({ limit: "1mb" }));
   app.use(express.urlencoded({ extended: false, limit: "1mb" }));
   app.set("view engine", "ejs");
