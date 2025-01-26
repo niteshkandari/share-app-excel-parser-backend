a6c39366cd5dec02766f1ce09879a5d37a38d44f
kandari_nitesh <niteshskandari@gmail.com>
2025-01-26 17:59:40 +0530

    updated vercel

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -2,25 +2,6 @@ import multer from "multer";
 import path from "path";
 import fs from "fs/promises";
 
-// Ensure the "uploads" directory exists, create it if it doesn't
-const createUploadsFolder = async () => {
-  try {
-    // Check if the directory exists
-    await fs.access("uploads"); // This will check if the directory exists
-    console.log("Uploads folder already exists.");
-  } catch (err:any) {
-    // If the folder does not exist, create it
-    if (err.code === "ENOENT") {
-      await fs.mkdir("uploads", { recursive: true });
-      console.log("Uploads folder created.");
-    } else {
-      console.error("Error checking or creating uploads folder:", err);
-    }
-  }
-};
-
-// Call the function to ensure the folder exists
-createUploadsFolder();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
